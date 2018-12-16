2f956a3d45416bdd842fe635f17b710d3b3a65ca
Kevan Sadeghi <ksadeghi27@gmail.com>
2018-12-16 23:14:38 +0100

    Version 0.1.4 Add feed twitter, cosmetics

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,36 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
 	public function index(Request $request) {
-		return view('home');
+		$settings = [
+			'oauth_access_token' => "471925848-JnPv1lmRB0o6JxAJlnCCklRYJKeUhm35yCU2OWi1",
+			'oauth_access_token_secret' => "ngIKT6rGaSUQ8xquhhToWuyxjEUwwbc1aR8wLXn4ZHqcn",
+			'consumer_key' => "7yE2muaMRY79stzYPQuuGFaIC",
+			'consumer_secret' => "5WSebgcTysuuTEp5ONmUDc6YdarJ1VVYneZooaReVuxL3KOkUN"
+		];
+
+		$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+		$getField = '?screen_name=SmashUltimateFR';
+		$requestMethod = 'GET';
+
+		$twitter = new \TwitterAPIExchange($settings);
+		$response = $twitter->setGetfield($getField)
+			->buildOauth($url, $requestMethod)
+			->performRequest();
+		$tweets = \GuzzleHttp\json_decode($response);
+		foreach ($tweets as $tweet) {
+			$timestamp = \DateTime::createFromFormat('D M d H:i:s O Y', $tweet->created_at);
+			$tweet->created_at = date('d F Y', $timestamp->getTimestamp());
+
+			// The Regular Expression filter
+			$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+			if(preg_match($reg_exUrl, $tweet->text, $url)) {
+				$tweet->text = preg_replace($reg_exUrl, "<a href=" . $url[0] . ">{$url[0]}</a> ", $tweet->text);
+			}
+		}
+//		dd($tweets);
+		return view('home', [
+			"tweets" => $tweets
+		]);
 	}
 
 	public function language(Request $request, $lang) {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,30 +5,53 @@
 @endsection
 
 @section("content")
-<div class="row my-4">
-	<div class="col-lg-3 border rounded p-3">
-		<div class="container">
-			<div class="row">
-				<h3 class="mx-auto">
-					<a href="{{ route('planning') }}" class="btn-link">
-						<i class="fas fa-calendar-alt"></i> {{ trans("home.calendar") }}
-					</a>
-				</h3>
-			</div>
-			<div class="row">
-				<div id="calendar" class="mx-auto"></div>
+	<div class="row my-4">
+		<div class="col-lg-3 border rounded p-3">
+			<div class="container">
+				<div class="row">
+					<h3 class="mx-auto">
+						<a href="{{ route('planning') }}" class="btn-link">
+							<i class="fas fa-calendar-alt"></i> {{ trans("home.calendar") }}
+						</a>
+					</h3>
+				</div>
+				<div class="row">
+					<div id="calendar" class="mx-auto"></div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="col-lg-6 col-sm-12 border rounded p-3">
-		<div class="container">
-			<div class="row">
-				<h3 class="mx-auto">
-					<a href="http://twitter.com/SmashUltimateFr">
-						<i class="fab fa-twitter"></i> {{ trans("home.twitter") }}
-					</a>
-				</h3>
+		<div class="col-lg-6 col-sm-12 border rounded p-3">
+			<div class="container">
+				<div class="row">
+					<h3 class="mx-auto">
+						<a href="http://twitter.com/SmashUltimateFr">
+							<i class="fab fa-twitter"></i> {{ trans("home.twitter") }}
+						</a>
+					</h3>
+				</div>
+				<div class="row">
+
+					@foreach($tweets as $tweet)
+						<div class="mx-auto">
+							<blockquote class="twitter-tweet" data-lang="{{ Session::get("locale", config('app.locale')) }}">
+								<p lang="{{ Session::get("locale", config('app.locale')) }}" dir="ltr">
+									{{ $tweet->text }}
+								</p>
+								&mdash; Smash Ultimate France (@SmashUltimateFR)
+								<a href="https://twitter.com/SmashUltimateFR/status/{{ $tweet->id_str }}">
+									{{ $tweet->created_at }}
+								</a>
+							</blockquote>
+						</div>
+					@endforeach
+
+				</div>
+			</div>
 		</div>
+		<div class="col-lg-3 col-sm-12 border rounded p-3">
+			<div class="row">
+				<h3 class="mx-auto">{{ trans("home.twitch") }}</h3>
+			</div>
 			<div class="row">
 				<img src="{{ asset("img/construction.png") }}" class="mx-auto d-block mt-5" alt="Under Construction">
 			</div>
@@ -37,27 +60,17 @@
 			</div>
 		</div>
 	</div>
-	<div class="col-lg-3 col-sm-12 border rounded p-3">
-		<div class="row">
-			<h3 class="mx-auto">{{ trans("home.twitch") }}</h3>
-		</div>
-		<div class="row">
-			<img src="{{ asset("img/construction.png") }}" class="mx-auto d-block mt-5" alt="Under Construction">
-		</div>
-		<div class="row">
-			<h3 class="mx-auto mt-3">{{ trans("home.construction") }}</h3>
-		</div>
-	</div>
-</div>
 @endsection
 
 @section('scripts')
-<script>
-	$('#calendar').fullCalendar({
+	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+	<script>
+		$('#calendar').fullCalendar({
 			locale: '{{ config('app.locale') }}',
 			googleCalendarApiKey: 'AIzaSyAqmSdhqBul-tb1irm1HnBF4YOSbqLY5-g',
 			events: {
-					googleCalendarId: 'pgduilavtc2m2ja7tiqvng413o@group.calendar.google.com'
+				googleCalendarId: 'pgduilavtc2m2ja7tiqvng413o@group.calendar.google.com'
 			},
 			header: {
 				left: 'prev',
@@ -86,25 +99,25 @@
 					return false;
 				}
 			},
-	});
+		});
 
-	function decodeHtmlSpecialCHars(text = "") {
-		var map = {
-			'&amp;': '&',
-			'&#038;': "&",
-			'&lt;': '<',
-			'&gt;': '>',
-			'&quot;': '"',
-			'&#039;': "'",
-			'&#8217;': "’",
-			'&#8216;': "‘",
-			'&#8211;': "–",
-			'&#8212;': "—",
-			'&#8230;': "…",
-			'&#8221;': '”'
-		};
+		function decodeHtmlSpecialCHars(text = "") {
+			var map = {
+				'&amp;': '&',
+				'&#038;': "&",
+				'&lt;': '<',
+				'&gt;': '>',
+				'&quot;': '"',
+				'&#039;': "'",
+				'&#8217;': "’",
+				'&#8216;': "‘",
+				'&#8211;': "–",
+				'&#8212;': "—",
+				'&#8230;': "…",
+				'&#8221;': '”'
+			};
 
-		return text.replace(/\&[\w\d\#]{2,5}\;/g, function(m) { return map[m]; });
-	}
-</script>
+			return text.replace(/\&[\w\d\#]{2,5}\;/g, function(m) { return map[m]; });
+		}
+	</script>
 @endsection
